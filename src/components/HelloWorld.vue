52c6f281058256dcfbdccec27973ba76d89f471c
Kearney <kearney.a@gmail.com>
2020-12-26 12:44:40 -0800

    First project update.

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="hello">
+    <h1 @click="onClick">{{ dataProperty }}</h1>
+    <input :value="dataProperty" @input="handleInput($event)"/>
+    <Media source="http://lorempixel.com/200/200/technics" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Media from './Media.vue';
+
+@Component({
+  components: {
+    Media,
+  },
+})
+export default class HelloWorld extends Vue {
+  dataProperty: string = 'started';
+
+  mounted() {
+    this.dataProperty = 'mounted'
+  }
+ 
+  onClick() {
+    this.dataProperty = 'clicked'
+  }
+
+  displayObject!: string;
+  
+  handleInput($event: InputEvent) {
+    let value;
+    if($event != null) {
+      value = $event.target.value;
+    }
+    console.log($event);
+    console.log(`value: ${value}`);
+    this.dataProperty = value;
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Media.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="media">
+        <img :src="`${source}`" />
+        {{source}}
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Media extends Vue {
+    @Prop({default: "hi"}) source!: string
+}
+</script>
+
+<style lang="scss">
+    .media {
+        color: $primary;
+
+        img {
+            display: block;
+            margin: 10px auto 5px;
+        }
+    }
+</style>
