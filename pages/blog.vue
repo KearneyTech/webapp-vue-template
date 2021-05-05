416386c325fb1c6253bc2ee2e76160a9f812fb53
Kearney Arceneaux <kalm@KALM.lan>
2021-05-04 22:59:10 -0700

    Blog post work, page layout updates.

--- pages/blog.vue
+++ pages/blog.vue
@@ -1,18 +1,32 @@
 <template>
   <section class="blog">
       <h1 class="heading">Blog</h1>
-      <div class="entry">
-          <h4 class="title">5/3 Entry</h4>
-          <p>
-              The plan is to make training material for a front end development project. I want to use it as content for website. I'll add it to my development plans. It's gonna take documentation to another level. If I can get the design system built, there should be plenty of topics for fairly large lesson. The training material can come in the form of video, presentation, and blog entry. This looks like the start of my re-education. Education could be a nice place to apply myself.
-          </p>
+      <div class="posts-wrapper">
+        <div class="entry">
+            <h4 class="title">5/3 Entry</h4>
+            <p>
+                The plan is to make training material for a front end development project. I want to use it as content for website. I'll add it to my development plans. It's gonna take documentation to another level. If I can get the design system built, there should be plenty of topics for fairly large lesson. The training material can come in the form of video, presentation, and blog entry. This looks like the start of my re-education. Education could be a nice place to apply myself.
+            </p>
+        </div>
+      </div>
+      <div v-for="post of posts" :key="post.title" class="posts-wrapper">
+        <div class="entry">
+            <h4 class="title">{{ post.title }}</h4>
+            <p>{{ post.content }}</p>
+        </div>
       </div>
   </section>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            posts: [
+                { title: "Blog Post Title", content: "Content goes here."}
+            ]
+        }
+    }
 }
 </script>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,110 @@
 <template>
-    <div class="main">
-        <h1>Home</h1>
-        <div class="nav">
-            <a href="https://www.github.com/KearneyTech" target="_blank">GitHub</a>
+    <div class="app">
+        <div class="app-header">
+            <div class="app-header-logo pane">
+                <h1>Web App</h1>
+            </div>
+            <div class="app-header-nav pane">
+                <h2>Menu</h2>
+                <a href="https://www.github.com/KearneyTech" target="_blank">GitHub</a>
+            </div>
         </div>
-        <div class="content">
+        <div class="app-content pane">
+            <h2>Welcome to my work</h2>
             <img src="images/pch.jpeg"/>
         </div>
     </div>
 </template>
 
 <style lang="scss">
-.main {
-    .content {
+.app {
+    background-color: $primary-background;
+    color: $primary-foreground;
+
+    @media (min-width: 992px) {
+        display: flex;
+        flex-direction: row;
+        align-items: stretch;
+        height: 100vh;
+    }
+
+    h1, h2 {
+        font-weight: 700;
+    }
+
+    h2 {
+        font-size: 1.3rem;
+        margin-top: .5rem;
+        margin-bottom: .8rem;
+    }
+
+    .pane {
+        //border: 1px solid #fff;
+        padding: .5rem;
+    }
+
+    &-header {
+        display: flex;
+        flex-direction: row;
+        height: 100px;
+
+        @media (min-width: 768px) {
+            height: 280px;
+        }
+
+        @media (min-width: 992px) {
+            height: auto;
+            flex-basis: 300px;
+        }
+
+        &-logo {
+            background-color: $contrast-background;
+            color: $contrast-foreground;
+            flex-basis: 120px;
+            text-align: center;
+
+            @media (min-width: 768px) {
+                flex-basis: auto;
+                flex-grow: 1;
+            }
+        }
+        &-nav {
+            background-color: $secondary-background;
+            color: $secondary-foreground;
+            flex-grow: 2;
+
+            @media (min-width: 768px) {
+                flex-grow: 1;
+            }
+
+            @media (min-width: 992px) {
+                height: auto;
+                flex-basis: 150px;
+            }
+
+            h2 {
+                display: inline-block;
+                padding-right: .8rem;
+            }
+
+            a,
+            a:focus,
+            a:visited,
+            a:hover {
+                color: $secondary-accent;
+            }
+        }
+    }
+    &-content {
+        background-color: $normal-background;
+        color: $normal-foreground;
+
         img {
             width: 100%
+        }
+
+        @media (min-width: 992px) {
+            overflow: scroll;
         }
     }
 }
