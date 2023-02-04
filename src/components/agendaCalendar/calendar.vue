1a4175eb322df63e88969b6e59929511e3862b8c
Kearney <email@kearneytech.com>
2023-02-04 11:44:11 -0800

    Small update

---
+++ src/components/agendaCalendar/calendar.vue
@@ -0,0 +1,20 @@
+<template>
+    <section class="calendar">
+        <div class="season-spring">
+            <h2>Spring</h2>
+            <div class="event">Hawaii?</div>
+            <div class="event">Jazz Fest?</div>
+        </div>
+        <div class="season-summer">
+            <h2>Summer</h2>
+            <div class="event">Atlanta?</div>
+        </div>
+        <div class="season-fall">
+            <h2>fall</h2>
+            <div class="event">Hawaii?</div>
+        </div>
+        <div class="season-winter">
+            <h2>winter</h2>
+        </div>
+    </section>
+</template>
