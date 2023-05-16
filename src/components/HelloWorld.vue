4a3f01c09fb0496772a92881aa87e06db7b64f7e
Kearney <email@kearneytech.com>
2023-05-16 00:32:38 -0700

    Added style template

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,105 @@
 <script setup lang="ts">
-import { ref } from "vue";
 
-
-const count = ref(0);
 </script>
 
 <template>
-  <section>
-    <div>
-      <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" class="logo" alt="Vite logo" />
-      </a>
-      <a href="https://vuejs.org/" target="_blank">
-        <img src="/vue.svg" class="logo vue" alt="Vue logo" />
-      </a>
+  <section class="template-glass">
+    <div class="logo">logo</div>
+    <div class="title">Glass Template</div>
+    <div class="header">Blocks</div>
+    <div class="block-container">
+      <div class="block">
+        BL
+      </div>
+      <div class="block">OC</div>
+      <div class="block">KS</div>
     </div>
-
-    <div class="card">
-      <button type="button" @click="count++">count is {{ count }}</button>
-      <p>
-        Edit
-        <code>components/HelloWorld.vue</code> to test HMR
-      </p>
+    <div class="header">Panels</div>
+    <div class="panel">
+          Edit to test HMR
     </div>
-
-    <p>
-      Check out
-      <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-        >create-vue</a
-      >, the official Vue + Vite starter
-    </p>
-    <p>
-      Install
-      <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-      in your IDE for a better DX
-    </p>
-    <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+    <div class="panel highlight">Reverse</div>
+    <div class="header">Text</div>
+    <div class="text">
+        Check out
+        <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
+          >create-vue</a
+        >, the official Vue + Vite starter
+      
+        Install
+        <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+        in your IDE for a better DX
+      Click on the Vite and Vue logos to learn more
+  </div>
 </section>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
+<style lang="scss" scoped>
+$thickness: 3px;
+$bottom-spacing: 4em;
+body {
+    font-family: 'Open Sans', sans-serif;
 }
-.logo {
+.template-glass {
+  margin: 2rem auto;
+  width: 60%;
+  max-width: 600px;
+}
+.logo{
+  width: 3em;
+  height: 3em;
+  border: 1px solid #fff;
+  margin: 0 auto;
+  margin-bottom: .5rem;
+}
+.link {
   height: 6em;
   padding: 1.5em;
   will-change: filter;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+.title {
+  display: inline;
+  font-size: 1.5rem;
+  font-weight: 700;
+  margin-bottom: $bottom-spacing;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.header {
+  font-size: 1.2rem;
+  margin: 1.8rem auto .75rem;
+  margin-bottom: .5em;
+}
+.block-container {
+  display: inline-flex;
+  justify-content: center;
+  width: 100%;
+  align-items: stretch;
+  flex-direction: row;
+  height: 8rem;
+}
+.block {
+  display: inline-flex;
+  border: $thickness solid #333232;
+  background-color: #333;
+  align-items: center;
+  justify-content: center;
+  flex-grow: 1;
+  margin: .4rem;
+}
+.panel{
+  display: inline-block;
+  border: $thickness solid #fff;
+  width: 100%;
+  margin: 1rem auto;
+  line-height: 3rem;
+}
+.text{
+  display: inline-block;
+  text-align: center;
+  margin: 0 auto;
+  margin-bottom: $bottom-spacing;
+}
+.highlight{
+  color: #000;
+  background-color: #fff;
 }
 </style>
