2a29e40feb29548fe41fe19a1a3f86e0adbf5209
Kearney <email@kearneytech.com>
2024-03-06 22:58:47 -0800

    New page

---
+++ src/components/Questions.vue
@@ -0,0 +1,30 @@
+<template>
+    <section class="questions">
+        <h3>Questions</h3>
+        <div><input type="text"/></div>
+        <div>
+            <select>
+                <option>First</option>
+                <option>Second</option>
+            </select>
+        </div>
+        <div>
+            <input name="radios" type="radio"><label>Almost Done</label>
+            <input name="radios" type="radio"><label>Done</label>
+        </div>
+    </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+
+});
+</script>
+
+<style>
+section.questions {
+    margin-top: 2rem;
+}
+</style>
