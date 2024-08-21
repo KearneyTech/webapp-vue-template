74f2e845993c8ac3ff694580e580c3004b2372bd
Kearney <email@kearneytech.com>
2024-08-20 20:48:06 -0700

    Cube tile clicks change color.

--- src/components/cube/Tile.vue
+++ src/components/cube/Tile.vue
@@ -1,16 +1,39 @@
 <template>
-    <div :id="configuration.id" :class="configuration.style" @click="handleClick"></div>
+    <div 
+        :id="configuration?.id" 
+        @click="handleClick"
+        :class="[
+            'border',
+            'border-slate-600',
+            'w-24',
+            'h-24',
+            configuration?.color
+        ]"
+    >
+        {{ configuration?.content }}
+    </div>
 </template>
 <script lang="ts">
-import defineComponent from 'vue';
+import {defineComponent} from 'vue';
+import type { PropType } from 'vue';
+
+type TileObject = {
+    id: string;
+    color: string;
+    style: string;
+    content: string;
+    clicked: boolean;
+};
 
 export default defineComponent({
+    name: 'Tile',
     props: {
-        configuration: Object
+        configuration: Object as PropType<TileObject>
     },
     methods: {
         handleClick() {
             // Emit click event
+            this.$emit('handleClick', this.configuration?.color);
         }
     }
 });
