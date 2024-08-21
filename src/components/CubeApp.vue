e1254a900ea0cb94f13d5cb91625e949abd9c9ab
Kearney <email@kearneytech.com>
2024-08-21 15:05:06 -0700

    Cube lifecycles, animations, tile refactor, styling.

--- src/components/CubeApp.vue
+++ src/components/CubeApp.vue
@@ -3,19 +3,18 @@
 * - Fix alignment of all page elements
 * - Create Board component
 * - Create Cube component
-* - Add photo background
-* - Create CSS animations
-* - Scene? location
+* - Add media background
 */
 <template>
     <div id="board" class="flex flex-col justify-center w-64">
-        <div id="cube" class="flex flex-wrap justify-center">
-            <Tile :configuration="tile1" @handleClick="handleTopLeftClick"/>
-            <Tile :configuration="tile2" @handleClick="handleTopRight"/>
-            <Tile :configuration="tile3" @handleClick="handleBottomLeft"/>
-            <Tile :configuration="tile4" @handleClick="handleBottomRight"/>
+        <p class="mb-8">the CUBE</p>
+        <div id="cube" :class="['flex', 'flex-wrap', 'justify-center', cubeStyle]">
+            <Tile :color="tile1.color" :id="tile1.id" @handleClick="handleTopLeftClick"/>
+            <Tile :color="tile2.color" :id="tile2.id" @handleClick="handleTopRight"/>
+            <Tile :color="tile3.color" :id="tile3.id" @handleClick="handleBottomLeft"/>
+            <Tile :color="tile4.color" :id="tile4.id" @handleClick="handleBottomRight"/>
         </div>
-        <p>Click tiles to change color. Match color of all tiles to solve the Cube.</p>
+        <p class="mt-8 text-slate-400">Click tiles to change color. Match color of all tiles to solve the Cube.</p>
     </div>
 </template>
 <script lang="ts">
@@ -40,6 +39,8 @@ export default defineComponent({
             tile2: {} as TileObject,
             tile3: {} as TileObject,
             tile4: {} as TileObject,
+            cubeSolved: false,
+            cubeStyle: "fade-in"
         }
     },
     mounted() {
@@ -75,49 +76,61 @@ export default defineComponent({
             clicked: false
         } as TileObject;
 
-        this.tile1.color = this.colors[this.randomNumber(4)];
-        this.tile2.color = this.colors[this.randomNumber(4)];
-
-        let randomIndex = this.randomNumber(4)
-        this.tile3.color = this.colors[randomIndex];
-
-        // Force next color to be different
-        randomIndex = (randomIndex === (this.colors.length - 1)) ? 0 : randomIndex + 1;
-        this.tile4.color = this.colors[randomIndex];
+        this.initCube();
     },
     methods: {
         initCube(){
-            // Randomly choose first tile's color
-            // Assign next tile color other than first, to prevent one color for all tiles
-            // Assign remaining tiles random colors
+            this.cubeSolved = false;
+            this.cubeStyle = "fade-out";
+            this.tile1.color = this.colors[this.randomNumber(4)];
+            this.tile2.color = this.colors[this.randomNumber(4)];
+
+            let randomIndex = this.randomNumber(4)
+            this.tile3.color = this.colors[randomIndex];
+
+            // Force next color to be different
+            randomIndex = (randomIndex === (this.colors.length - 1)) ? 0 : randomIndex + 1;
+            this.tile4.color = this.colors[randomIndex];
+            setTimeout(this.cubeStyle = "fade-in", 1500);
         },
         evaluateCube(){
             // If all tiles are the same color, the cube is solved
+            if(
+                this.tile1.color === this.tile2.color &&
+                this.tile1.color === this.tile3.color &&
+                this.tile1.color === this.tile4.color
+            ) {
+                this.handleCubeSolved();
+            }
         },
         handleCubeSolved(){
-            // Initialize next cube
-            // Remove solved cube
+            this.cubeStyle = "rotate-center";
+            setTimeout(()=>{this.initCube()}, 2500);
         },
         handleTopLeftClick() {
             const newColor = this.nextColor(this.tile1.color);
             this.tile1.color = newColor;
+            this.evaluateCube();
+        },
+        handleTopRight(){
+            const newColor = this.nextColor(this.tile2.color);
+            this.tile2.color = newColor;
+            this.evaluateCube();
+        },
+        handleBottomLeft(){
+            const newColor = this.nextColor(this.tile3.color);
+            this.tile3.color = newColor;
+            this.evaluateCube();
+        },
+        handleBottomRight(){
+            const newColor = this.nextColor(this.tile4.color);
+            this.tile4.color = newColor;
+            this.evaluateCube();
         },
         nextColor(color: string){
             let colorIndex = this.colors.indexOf(color);
             colorIndex = (colorIndex === (this.colors.length - 1)) ? 0 : colorIndex + 1;
             return this.colors[colorIndex];
-        },
-        handleTopRight(){
-            const newColor = this.nextColor(this.tile2.color);
-            this.tile2.color = newColor;
-        },
-        handleBottomLeft(){
-            const newColor = this.nextColor(this.tile3.color);
-            this.tile3.color = newColor;
-        },
-        handleBottomRight(){
-            const newColor = this.nextColor(this.tile4.color);
-            this.tile4.color = newColor;
         },
         randomNumber(max: number){
             return Math.floor(Math.random() * max);
@@ -147,4 +160,72 @@ section.app-vue {
         background-color: white;
     }
 }
+.fade-in {
+	-webkit-animation: fade-in 1.2s cubic-bezier(0.390, 0.575, 0.565, 1.000) both;
+	        animation: fade-in 1.2s cubic-bezier(0.390, 0.575, 0.565, 1.000) both;
+}
+.rotate-center {
+	-webkit-animation: rotate-center 0.6s ease-in-out both;
+	        animation: rotate-center 0.6s ease-in-out both;
+}
+.fade-out {
+	-webkit-animation: fade-out 1s ease-out both;
+	        animation: fade-out 1s ease-out both;
+}
+
+@-webkit-keyframes fade-in {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+@keyframes fade-in {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
+@-webkit-keyframes rotate-center {
+  0% {
+    -webkit-transform: rotate(0);
+            transform: rotate(0);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+            transform: rotate(360deg);
+  }
+}
+@keyframes rotate-center {
+  0% {
+    -webkit-transform: rotate(0);
+            transform: rotate(0);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+            transform: rotate(360deg);
+  }
+}
+
+@-webkit-keyframes fade-out {
+  0% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+}
+@keyframes fade-out {
+  0% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+}
+
 </style>
